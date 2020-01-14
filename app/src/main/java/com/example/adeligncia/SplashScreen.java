1778dd117c1155e5de9275c96c7d88e3c809c22e
CleitonBoeno <cboeno1822@gmail.com>
2020-01-13 23:07:01 -0300

    implementacion

--- app/src/main/java/com/example/adeligncia/SplashScreen.java
+++ app/src/main/java/com/example/adeligncia/SplashScreen.java
@@ -2,6 +2,8 @@ package com.example.adeligncia;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,7 +11,14 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_scren);
 
-        startActivity(new Intent(this,MainActivity.class));
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(),MainActivity.class));
+                finish();
+            }
+        },5000);
     }
 }
