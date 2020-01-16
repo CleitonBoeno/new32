be897010e03966f5a31944371c6dfee21e0b4971
Visitante7 <cboeno1822@gmail.com>
2020-01-16 10:34:22 -0300

    ok

--- app/src/main/java/com/example/adeligncia/cadastrar.java
+++ app/src/main/java/com/example/adeligncia/cadastrar.java
@@ -3,6 +3,7 @@ package com.example.adeligncia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
