bc67bfa063a9aa70101d560bdac5d0a87c8c68ff
Visitante7 <cboeno1822@gmail.com>
2020-01-28 10:03:05 -0300

    0

---
+++ app/src/main/java/com/example/adeligncia/testegit.java
@@ -0,0 +1,14 @@
+package com.example.adeligncia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class testegit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testegit);
+    }
+}
